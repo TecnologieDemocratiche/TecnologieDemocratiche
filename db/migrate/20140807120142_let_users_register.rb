a586859b762e46dc7da7d040b442238117f9abd0
Alessandro Rodi <alessandro.rodi@renuo.ch>
2014-08-08 17:18:56 +0200

    all approved

--- db/migrate/20140807120142_let_users_register.rb
+++ db/migrate/20140807120142_let_users_register.rb
@@ -5,5 +5,7 @@ class LetUsersRegister < ActiveRecord::Migration
     add_column :users, :confirmation_sent_at, :datetime
     add_column :users, :approved, :boolean, :default => false, :null => false
     add_index  :users, :approved
+
+    User.update_all(approved: true)
   end
 end
