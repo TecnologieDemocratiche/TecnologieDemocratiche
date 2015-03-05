2d5e4834414f55924f12bcea2f652670876d0696
Alessandro Rodi <alessandro.rodi@renuo.ch>
2015-03-06 00:08:18 +0100

    remove old file and duplicated code

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,18 +1,3 @@
 module ApplicationHelper
 
-  def email_panel
-    content_tag :table, class: 'row callout' do
-      content_tag :tr do
-        content_tag :td, class: 'wrapper last' do
-          content_tag :table, class: 'twelve columns' do
-            content_tag :tr do
-              content_tag :td, class: 'panel' do
-                yield
-              end
-            end
-          end
-        end
-      end
-    end
-  end
 end
