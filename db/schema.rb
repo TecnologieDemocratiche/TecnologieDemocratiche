3890313bd4d867ff86dd1068a1597f4073142b50
Alessandro Rodi <alessandro.rodi@renuo.ch>
2015-03-26 16:39:56 +0100

    we don't need plpgsql for td app

--- db/schema.rb
+++ db/schema.rb
@@ -12,10 +12,7 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 20150225181832) do
-
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
+  
   create_table "oauth_access_grants", force: :cascade do |t|
     t.integer  "resource_owner_id",             null: false
     t.integer  "application_id",                null: false
