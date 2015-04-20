f3491c1fe01c7af67ead6c7f81e9c497b5bda28d
Lorenzo <lorenzo.angelini3@gmail.com>
2015-04-20 09:48:51 +0200

    removed unlock instructions mailer view

--- spec/mailers/previews/devise/mailer_preview.rb
+++ spec/mailers/previews/devise/mailer_preview.rb
@@ -8,7 +8,4 @@ class Devise::MailerPreview < ActionMailer::Preview
     Devise::Mailer.reset_password_instructions(User.first, "faketoken")
   end
 
-  def unlock_instructions
-    Devise::Mailer.unlock_instructions(User.first, "faketoken")
-  end
 end
