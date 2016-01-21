4acf78b04ee8313d2dbe99140bcf4fcebac4e99c
Alessandro Rodi <alessandro.rodi@renuo.ch>
2016-01-21 22:43:31 +0100

    change paperclip variables

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,11 +83,13 @@ Rails.application.configure do
 
   config.paperclip_defaults = {
     storage: :s3,
-    s3_permissions: :private,
+    s3_protocol: 'https',
+    bucket: ENV['S3_BUCKET_NAME'],
     s3_credentials: {
-      bucket: ENV['AWS_BUCKET'],
       access_key_id: ENV['AWS_ACCESS_KEY_ID'],
-      secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
-    }
+      secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+      s3_signature_version: :v4
+    },
+    s3_host_name: ENV['S3_HOST_NAME']
   }
 end
